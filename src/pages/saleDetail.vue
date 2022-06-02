d75954f0374258a77fd0e5b25ec44b7aef0cfa2c
DaniCis <danicisneros2@hotmail.com>
2022-06-01 23:05:59 -0500

    image size in sale detail

--- src/pages/saleDetail.vue
+++ src/pages/saleDetail.vue
@@ -9,44 +9,18 @@
                 <div class="flex flex-col justify-start items-start w-full space-y-4 md:space-y-6 xl:space-y-8">
                     <div class="flex flex-col justify-start items-start dark:bg-gray-800 bg-gray-50 px-4 py-4 md:py-6 md:p-6 xl:p-8 w-full">
                         <p class="text-lg md:text-xl dark:text-white font-semibold leading-6 xl:leading-5 text-gray-800">Detalles de Compra</p>
-                        <div class="mt-4 md:mt-6 flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full">
-                            <div class="pb-4 md:pb-8 w-full md:w-40">
-                                <img class="w-full hidden md:block" src="https://i.ibb.co/84qQR4p/Rectangle-10.png" alt="dress" />
+                        <div  v-for="product in this.detalles" :key="product.id_producto" class="mt-4 md:mt-6 flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full">
+                            <div class="pb-4 md:w-40">
+                                <img class="mt-5 md:mt-0" :src="`http://10.147.17.173:5002/productos/images_small/${product.id_producto}/${product.imagen_producto[0]}`" :alt="product.nombre_producto"/>
                             </div>
-                            <div class="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full pb-8 space-y-4 md:space-y-0">
+                            <div class="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full pb-4 space-y-4 md:space-y-0">
                                 <div class="w-full flex flex-col justify-start items-start space-y-8">
-                                    <h3 class="text-xl dark:text-white xl:text-2xl font-semibold leading-6 text-gray-800">Premium Quaility Dress</h3>
-                                    <div class="flex justify-start items-start flex-col space-y-2">
-                                        <p class="text-sm dark:text-white leading-none text-gray-800"><span class="dark:text-gray-400 text-gray-300">Style: </span> Italic Minimal Design</p>
-                                        <p class="text-sm dark:text-white leading-none text-gray-800"><span class="dark:text-gray-400 text-gray-300">Size: </span> Small</p>
-                                        <p class="text-sm dark:text-white leading-none text-gray-800"><span class="dark:text-gray-400 text-gray-300">Color: </span> Light Blue</p>
-                                    </div>
+                                    <h3 class="text-xl dark:text-white xl:text-1xl font-semibold leading-6 text-gray-600">{{product.nombre_producto}}</h3>
                                 </div>
                                 <div class="flex justify-between space-x-8 items-start w-full">
-                                    <p class="text-base dark:text-white xl:text-lg leading-6">$36.00 <span class="text-red-300 line-through"> $45.00</span></p>
-                                    <p class="text-base dark:text-white xl:text-lg leading-6 text-gray-800">01</p>
-                                    <p class="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800">$36.00</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="mt-6 md:mt-0 flex justify-start flex-col md:flex-row items-start md:items-center space-y-4 md:space-x-6 xl:space-x-8 w-full">
-                            <div class="w-full md:w-40">
-                                <img class="w-full hidden md:block" src="https://i.ibb.co/s6snNx0/Rectangle-17.png" alt="dress" />
-                                <img class="w-full md:hidden" src="https://i.ibb.co/BwYWJbJ/Rectangle-10.png" alt="dress" />
-                            </div>
-                            <div class="flex justify-between items-start w-full flex-col md:flex-row space-y-4 md:space-y-0">
-                                <div class="w-full flex flex-col justify-start items-start space-y-8">
-                                    <h3 class="text-xl dark:text-white xl:text-2xl font-semibold leading-6 text-gray-800">High Quaility Italic Dress</h3>
-                                    <div class="flex justify-start items-start flex-col space-y-2">
-                                        <p class="text-sm dark:text-white leading-none text-gray-800"><span class="dark:text-gray-400 text-gray-300">Style: </span> Italic Minimal Design</p>
-                                        <p class="text-sm dark:text-white leading-none text-gray-800"><span class="dark:text-gray-400 text-gray-300">Size: </span> Small</p>
-                                        <p class="text-sm dark:text-white leading-none text-gray-800"><span class="dark:text-gray-400 text-gray-300">Color: </span> Light Blue</p>
-                                    </div>
-                                </div>
-                                <div class="flex justify-between space-x-8 items-start w-full">
-                                    <p class="text-base dark:text-white xl:text-lg leading-6">$20.00 <span class="text-red-300 line-through"> $30.00</span></p>
-                                    <p class="text-base dark:text-white xl:text-lg leading-6 text-gray-800">01</p>
-                                    <p class="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800">$20.00</p>
+                                    <p v-if="product.descuentoPorcentaje_detalleVenta!=0" class="text-base dark:text-white xl:text-lg leading-6">Desct. <span class="text-red-300">%{{product.descuentoPorcentaje_detalleVenta}}</span></p>
+                                    <p class="text-base dark:text-white xl:text-lg leading-6 text-gray-800">{{product.cantidad_detalleVenta}}</p>
+                                    <p class="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800">{{product.precio_detalleVenta}}</p>
                                 </div>
                             </div>
                         </div>
@@ -85,7 +59,6 @@
                                     <p class="text-sm dark:text-gray-300 leading-5 text-gray-600">{{this.identCliente}}</p>
                                 </div>
                             </div>
-    
                             <div class="flex justify-center text-gray-800 dark:text-white md:justify-start items-center space-x-4 py-4 border-b border-gray-200 w-full">
                                 <i class="pi pi-user"></i>
                                 <p class="cursor-pointer text-sm leading-5 ">{{this.nombreCliente}}</p>
@@ -129,7 +102,6 @@ export default {
             this.iva = parseFloat(subt) - parseFloat(desc)
             this.iva = (this.iva*0.12).toFixed(2)
         },
-
         async getVenta(ventaId){
             await this.axios.get(`http://10.147.17.173:5004/public/ventaDetalle/${ventaId}`,{ headers:{ Authorization: 'Bearer ' + getAccessToken() }
             }).then(response => {
@@ -147,7 +119,6 @@ export default {
                 this.$toast.add({severity:'Error', summary:'Error', detail: e.response.data.detail, life: 3000})
             })
         },
-    
     }
 }
 </script>

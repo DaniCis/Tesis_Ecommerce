2f91fec5ed7f3e0d0f755f04fecfeba0271b1784
DaniCis <danicisneros2@hotmail.com>
2022-08-07 22:17:36 -0500

    change server

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 3001,
+    host:'10.147.17.71',
   }
 })
